cc232accd124d9d6264dbf4f70b999936e5be236
Jose Fernandez <josesuldez@gmail.com>
2020-08-23 13:34:14 -0400

    Updates

---
+++ precache-manifest.4d1b2927942d7edede1b65a9118ba5ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54df584d0ee6577d54dff1b4cf2e0f42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "184cdd26e27b89b427f4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c3a4f187a583c0f56da7",
+    "url": "/static/js/2.b3742f05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b3742f05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "184cdd26e27b89b427f4",
+    "url": "/static/js/main.88314a09.chunk.js"
+  },
+  {
+    "revision": "8a8d0b20f22d46b49646",
+    "url": "/static/js/runtime-main.035faa04.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
