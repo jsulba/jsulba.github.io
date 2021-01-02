0b762befcabcb0d9a01ecd4f5767ded335bf3e06
Jose Fernandez <josesuldez@gmail.com>
2021-01-01 20:36:23 -0500

    Added link to SCS website and finished About Me descriptions

--- src/components/about_me.js
+++ src/components/about_me.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Divider, GridList, GridListTile } from '@material-ui/core';
+import { Grid, Divider, GridList, GridListTile, ButtonGroup, Button, createMuiTheme } from '@material-ui/core';
+import OpenInNewIcon from '@material-ui/icons/OpenInNew';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import { grey } from '@material-ui/core/colors';
 import { paragraphs, about_images } from './info_objects';
 
 const useStyles = makeStyles((theme) => ({
@@ -8,6 +12,13 @@ const useStyles = makeStyles((theme) => ({
       flexGrow: 1,
       marginTop: "5%",
       maxWidth: "100%",
+    },
+    link: {
+      color: "white",
+      textDecoration: "none",
+      fontSize: "0.9em",
+      paddingRight: "2em",
+      fontWeight: 600
     },
     menuButton: {
       marginRight: theme.spacing(2),
@@ -40,8 +51,36 @@ const useStyles = makeStyles((theme) => ({
       maxWidth: "95%",
       height: "90%",
       marginTop: "25%"
+    },
+    footer: {
+      marginTop: "5em",
+      paddingTop: "2em",
+      paddingBottom: "1em",
+      backgroundImage: "linear-gradient(rgba(13, 15, 128, 0.781), rgb(9, 13, 226), rgba(7, 9, 128, 0.881))",
+      width: "100%",
+      minHeight: "100px",
+      marginLeft: "auto",
+      marginRight: "auto",
+      display: "flex",
+      alignItems: "center",
+      color: "white"
+    },
+    footerButtonGroup: {
+      color: "white",
+      marginLeft: "auto",
+      marginRight: "auto",
+      overflow: "hidden",
+    },
+    footerButton: {
+      color: "white",
     }
 }));
+
+const theme = createMuiTheme({
+  palette: {
+    primary: grey 
+  }
+});
 
 export default function AboutMe() {
     const classes = useStyles();
@@ -69,10 +108,17 @@ export default function AboutMe() {
               </div>
             </Grid>
             <Grid item xs={2}>
-              
+              {/* empty spacer */}
             </Grid>
           </Grid>
         ))}
+        <div id="footer" className={classes.footer}>
+          <ButtonGroup className={classes.footerButtonGroup} variant="text">
+              <a className={classes.link} href="https://github.com/jsulba" target="_blank"><Button theme={theme}><GitHubIcon className={classes.footerButton}/></Button>Github</a>
+              <a className={classes.link} href="https://www.linkedin.com/in/jose-ale-fernandez/" target="_blank"><Button theme={theme}><LinkedInIcon className={classes.footerButton} /></Button>LinkedIn</a>
+              <a className={classes.link} href="https://scsconcordia.com/" target="_blank"><Button ><OpenInNewIcon className={classes.footerButton} /></Button>SCS Website</a>
+          </ButtonGroup>
+        </div>
       </div>
     );
 }
